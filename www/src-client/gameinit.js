458442711a78640e05aee5d0e7145278939301d6
Leonardo Silveira <sombriks@gmail.com>
2016-07-17 14:16:29 -0300

    fix screen resize

--- www/src-client/gameinit.js
+++ www/src-client/gameinit.js
@@ -1,6 +1,6 @@
 var GameMap = require("game/gamemap.js");
 
-var renderer = new PIXI.WebGLRenderer(320, 240, {
+var renderer = new PIXI.WebGLRenderer(480, 480, {
 	transparent : true
 });
 document.body.appendChild(renderer.view);
@@ -12,7 +12,10 @@ gm.givePlayer(2, 2);// começamos com uma casa
 function resize() {
 	var width = document.body.clientWidth;
 	var height = document.body.clientHeight;
-	renderer.resize(width, height);
+	var x = width;
+	if (width > height)
+		x = height;
+	renderer.resize(x, x);
 	gm.resize(width, height);
 }
 
